b91c400be51d5bf4b76928f9073995bf780f4406
Kai Schmidt <kaikaliischmidt@gmail.com>
2018-05-02 12:38:09 -0700

    added a samaple window so that samples are proccessed in well-defined batches

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ fn main() {
     let mut args = env::args();
     args.next();
     let mut sample_rate = 32000.0;
+    let mut window_size = 100;
     let mut file_name = None;
     while let Some(ref arg) = args.next() {
         match arg.to_string().as_ref() {
@@ -30,6 +31,14 @@ fn main() {
                     sample_rate = sr;
                 } else {
                     println!("Invalid sample rate.");
+                    return;
+                }
+            },
+            "-w" | "--window" => if let Some(ref w_str) = args.next() {
+                if let Ok(w) = w_str.parse() {
+                    window_size = w;
+                } else {
+                    println!("Invalid window size.");
                     return;
                 }
             },
@@ -43,6 +52,7 @@ Options:
     -h | --help             Display this message
     -r | --sample_rate      Set the sample rate of the output file
                             in samples/second (default is 32000)
+	-w | --window			Set the size of the processing window
 "
                 );
                 return;
@@ -55,7 +65,7 @@ Options:
             Ok(parser) => match parser.parse(false) {
                 Ok(mut builder) => {
                     builder.make_functions();
-                    write(builder, sample_rate);
+                    write(builder, sample_rate, window_size);
                 }
                 Err(error) => error.report(),
             },
@@ -66,7 +76,7 @@ Options:
     }
 }
 
-fn write(builder: Builder, sample_rate: f64) {
+fn write(builder: Builder, sample_rate: f64, window_size: usize) {
     // Find the audio end time
     // TODO: make this get done on a per-outchain basis
     let mut end: f64 = 1.0;
@@ -76,16 +86,23 @@ fn write(builder: Builder, sample_rate: f64) {
         }
     }
     end = end.max(builder.end_time);
+
+    // output each outchain
     for name in builder.chains.iter().filter(|f| f.1.play).map(|f| f.0) {
         let mut song = vec![(0f64, 0usize); (sample_rate * end) as usize];
         for (i, (_, ref mut x)) in song.iter_mut().enumerate() {
             *x = i;
         }
+        for window_start in (0..(song.len() / window_size)).map(|x| x * window_size) {
+            song[window_start..(window_start + window_size)]
+                .par_iter_mut()
+                .for_each(|(sample, i)| {
+                    let time = *i as f64 / sample_rate;
+                    *sample = builder.evaluate_function(&name, &[], time, 0);
+                });
+        }
 
-        song.par_iter_mut().for_each(|(sample, i)| {
-            let time = *i as f64 / sample_rate;
-            *sample = builder.evaluate_function(&name, &[], time, 0);
-        });
+        // Write the audio file
         let spec = hound::WavSpec {
             channels: 1,
             sample_rate: sample_rate as u32,
